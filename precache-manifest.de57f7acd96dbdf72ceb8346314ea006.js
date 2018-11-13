c63a2a0be6335faa786b4edbca4ac72039b01c23
Anthony Costanzo <a.e.j.costanzo@gmail.com>
2018-11-13 13:37:39 -0500

    Initial build subtree commit

---
+++ precache-manifest.de57f7acd96dbdf72ceb8346314ea006.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6271b133a85778ba904a",
+    "url": "/static/js/main.6271b133.chunk.js"
+  },
+  {
+    "revision": "3ca09be90e68024411fe",
+    "url": "/static/js/1.3ca09be9.chunk.js"
+  },
+  {
+    "revision": "6271b133a85778ba904a",
+    "url": "/static/css/main.c9bfad03.chunk.css"
+  },
+  {
+    "revision": "3ca09be90e68024411fe",
+    "url": "/static/css/1.375ba187.chunk.css"
+  },
+  {
+    "revision": "bb5272207bf9d0377370ac1ada2fb6a3",
+    "url": "/index.html"
+  }
+];
