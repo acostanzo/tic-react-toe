b5b7a7304c951c6b5e56202625942144b3cb1468
Anthony Costanzo <a.e.j.costanzo@gmail.com>
2018-11-13 13:49:06 -0500

    Updates

---
+++ precache-manifest.dc079231fc09b63f17accfa0cde61194.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e36dd5e4cf45fc7e1680",
+    "url": "/tic-react-toe/static/js/runtime~main.e36dd5e4.js"
+  },
+  {
+    "revision": "6271b133a85778ba904a",
+    "url": "/tic-react-toe/static/js/main.6271b133.chunk.js"
+  },
+  {
+    "revision": "3ca09be90e68024411fe",
+    "url": "/tic-react-toe/static/js/1.3ca09be9.chunk.js"
+  },
+  {
+    "revision": "6271b133a85778ba904a",
+    "url": "/tic-react-toe/static/css/main.c9bfad03.chunk.css"
+  },
+  {
+    "revision": "3ca09be90e68024411fe",
+    "url": "/tic-react-toe/static/css/1.375ba187.chunk.css"
+  },
+  {
+    "revision": "00f13826105447af0da2685fd7d22358",
+    "url": "/tic-react-toe/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.de57f7acd96dbdf72ceb8346314ea006.js"
+  "/tic-react-toe/precache-manifest.dc079231fc09b63f17accfa0cde61194.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/tic-react-toe/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
